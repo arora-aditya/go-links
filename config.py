c660229cb277bc0b41058f8e0af85b9b0cc75c36
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2021-12-25 13:27:01 -0800

    Add analytics, fix create link workflow

--- config.py
+++ config.py
@@ -1,17 +1,21 @@
 from configparser import ConfigParser
 
+CONFIG_FILE = "config.ini"
 
-def parse_config():
+def parse_config(file_path: str):
     config = ConfigParser()
-    config.read("config.ini")
+    config.read(CONFIG_FILE)
     return config
 
 
-config = parse_config()
+config = parse_config(CONFIG_FILE)
 
 SECTION = "DEFAULT"
 MAPPING_FILE = config.get(SECTION, "MAPPING_FILE")
 METRICS_FILE = config.get(SECTION, "METRICS_FILE")
 CERTIFICATE_FILE = config.get(SECTION, "CERTIFICATE_FILE")
 NEW_LINK_PREFIX = config.get(SECTION, "NEW_LINK_PREFIX")
+DELETE_LINK_PREFIX = config.get(SECTION, "DELETE_LINK_PREFIX")
+ANALYTICS_LINK_PREFIX = config.get(SECTION, "ANALYTICS_LINK_PREFIX")
+TEST_LINK_PREFIX = config.get(SECTION, "TEST_LINK_PREFIX")
 PORT = int(config.get(SECTION, "PORT"))
--- mapping.py
+++ mapping.py
@@ -8,10 +8,10 @@ from urllib.parse import parse_qs
 META = """<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
 <meta content="utf-8" http-equiv="encoding">"""
 # default styling taken from http://bettermotherfuckingwebsite.com/
-STYLE = """<style>body{margin:40px auto;max-width:650px;line-height:1.6;font-size:18px;color:#444;padding:0 10px}h1,h2,h3{line-height:1.2}</style>"""
+STYLE = """<style>body{font-family:-apple-system,BlinkMacSystemFont,sans-serif;margin:40px auto;max-width:650px;line-height:1.6;font-size:18px;color:#444;padding:0 10px}h1,h2,h3{line-height:1.2;font-weight:normal;}a{text-decoration:none;}a:visited{color:#00E;}</style>"""
 
 
-def uri_validator(x):
+def uri_validator(x: str):
     try:
         result = urlparse(x)
         return all([result.scheme, result.netloc])
@@ -20,7 +20,7 @@ def uri_validator(x):
 
 
 class Mapping:
-    def __init__(self, path):
+    def __init__(self, path: str):
         self.mapping = {}
         self._path = path
         self.load()
@@ -33,28 +33,38 @@ class Mapping:
         with open(self._path, "w") as f:
             f.write(dumps(self.mapping, sort_keys=True, indent=4))
 
-    def get(self, key):
+    def get(self, key: str):
         s = key.split("/")
         if s[0] in self.mapping:
             return urljoin(self.mapping[s[0]], "/".join(s[1:]))
         return None
 
-    def set(self, key, value):
+    def set(self, key: str, value: str):
         if uri_validator(value):
             self.mapping[key] = value
             self.save()
 
-    def set_from_qs(self, qs):
+    def set_from_qs(self, qs: str):
         response = ""
         for key, value in parse_qs(qs).items():
             value = value[0].strip()
             key = key.strip()
             self.set(key, value)
-            response += "{} -> {}\n".format(key, value)
+            if uri_validator(value):
+                response += f"{key} -> {value}<br />"
+            else:
+                response += f"{key} -> {value} (invalid)<br />"
         return response
+    
+    def delete(self, key: str):
+        if key in self.mapping:
+            del self.mapping[key]
+            self.save()
+            return f"Deleted {key}"
+        return f"No mapping found for {key}"
 
-    def __str__(self):
-        s = META + STYLE
+    def __str__(self) -> str:
+        s = ""
         for key, value in self.mapping.items():
             s += f"<h3><a href='{value}' target='_blank'>{key}</a> - {value}</h3>"
         return s
